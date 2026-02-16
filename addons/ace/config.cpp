3b409e14a7e00e08a82d5a14a5b3260a219de427
Lupus590 <9790321+Lupus590@users.noreply.github.com>
2026-02-16 01:19:19 +0000

    now that we have a keybind we dont actually need to depend on ace (#7)
    * move insignia to its own addon folder
    
    * changelog
    
    * split off the ace interact into it's own addon
    
    * vanilla dependency
    
    * fix format
    
    * woops, ace part should be optional
    
    * tidy up
    
    * chore: fix .editorconfig issues automatically
    
    * whitespace
    
    * change addon name
    
    * make ace optional
    
    * add icon to the interact
    
    * oops
    
    * changelog
    
    * woops
    
    * Automated prep release

---
+++ addons/ace/config.cpp
@@ -0,0 +1,41 @@
+class CfgPatches
+{
+	class HFGB_ACE
+	{
+		name = "Headpats For Good Beans - ACE";
+		author = "Lupus590";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] =
+		{
+			"HFGB_Core",
+			"HFGB_Insignia",
+			"tsp_animate_ace",
+			"ace_interaction",
+		};
+		skipWhenMissingDependencies = 1;
+	};
+};
+
+class CfgVehicles
+{
+	class Man;
+	class CAManBase: Man
+	{
+	class ACE_Actions
+		{
+			class ACE_TapShoulderRight;
+			class ACE_Head
+			{
+				class ACE_PatHead : ACE_TapShoulderRight
+				{
+					displayName = "Pat Head";
+					selection = "";
+					condition = "[_player, _target] call ace_interaction_fnc_canTapShoulder && tsp_cba_animate_tap";
+					statement = "[_player,_target] spawn hfgb_fnc_patHead";
+					icon = "\z\HFGB\addons\insignia\HFGB_patch.paa";
+				};
+			};
+		};
+	};
+};
